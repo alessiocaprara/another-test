3861aad441240dbcd9c61433f96366826fd830b8
Alessio <capraraalessio@gmail.com>
2023-12-02 20:25:49 +0100

    My First serious commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            { hostname: "lh3.googleusercontent.com" },
+            { hostname: "tailwindui.com" },
+            { hostname: "localhost" },
+        ]
+    }
+}
 
 module.exports = nextConfig
